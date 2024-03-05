80b9730defb8ca7a1c267397ba5f2cd996170ea7
tesloach ker <127243762+tesloachkerchan@users.noreply.github.com>
2024-03-05 20:38:08 +0300

    m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
+import "./App.css";
+import { Router,Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Cart from "./components/Cart";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <>
-      <div>
-        helo
-      </div>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <div className="content-container">
+          <Routes>
+            <Route path="/cart" element={Cart} />
+            <Route path="/not-found" element={NotFound} />
+            <Route path="/" exact element={Home} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+
+  return (
+    <div className="home-container">
+      hi
+    </div>
+  );
+};
+
+export default Home;
